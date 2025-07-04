068bd128938822707d59d7249d4eed4d6cab21d7
Jeremi <jeremics97@gmail.com>
2025-07-04 14:31:02 -0500

    docventas_bus

--- iac/modules/events/main.tf
+++ iac/modules/events/main.tf
@@ -1,11 +1,11 @@
-resource "aws_cloudwatch_event_bus" "ordenes_bus" {
+resource "aws_cloudwatch_event_bus" "docventas_bus" {
     name = "ordenes-bus"
 }
 
 resource "aws_cloudwatch_event_rule" "crear_docventa" {
     name           = "crear-docventa"
     description    = "Regla para crear orden desde evento personalizado"
-    event_bus_name = aws_cloudwatch_event_bus.ordenes_bus.name
+    event_bus_name = aws_cloudwatch_event_bus.docventas_bus.name
     event_pattern = jsonencode({
         source       = ["pe.com.jeress"],
         "detail-type": ["crear-docventa"]
@@ -16,7 +16,7 @@ resource "aws_cloudwatch_event_target" "target_lambda_crear_docventa" {
     rule      = aws_cloudwatch_event_rule.crear_docventa.name
     target_id = "crear-docventa-lambda"
     arn       = var.crear_docventa_funcion_arn
-    event_bus_name = aws_cloudwatch_event_bus.ordenes_bus.name
+    event_bus_name = aws_cloudwatch_event_bus.docventas_bus.name
 }
 
 resource "aws_lambda_permission" "allow_eventbridge" {
@@ -30,7 +30,7 @@ resource "aws_lambda_permission" "allow_eventbridge" {
 resource "aws_cloudwatch_event_rule" "procesar_carrito" {
     name           = "procesar-carrito"
     description    = "Regla para procesar carrito desde evento personalizado"
-    event_bus_name = aws_cloudwatch_event_bus.ordenes_bus.name
+    event_bus_name = aws_cloudwatch_event_bus.docventas_bus.name
     event_pattern = jsonencode({
         source       = ["pe.com.jeress"],
         "detail-type": ["procesar-carrito"]
@@ -41,7 +41,7 @@ resource "aws_cloudwatch_event_target" "target_lambda_procesar_carrito" {
     rule      = aws_cloudwatch_event_rule.procesar_carrito.name
     target_id = "procesar-carrito-lambda"
     arn       = var.procesar_carrito_funcion_arn
-    event_bus_name = aws_cloudwatch_event_bus.ordenes_bus.name
+    event_bus_name = aws_cloudwatch_event_bus.docventas_bus.name
 }
 
 resource "aws_lambda_permission" "allow_eventbridge_procesar_carrito" {
