4614e4a3edd4283c751f4f675310fbe609f4aecd
Jeremi <jeremics97@gmail.com>
2025-07-04 14:41:39 -0500

    procesacarrito_bus

--- iac/modules/events/main.tf
+++ iac/modules/events/main.tf
@@ -27,10 +27,14 @@ resource "aws_lambda_permission" "allow_eventbridge" {
     source_arn    = aws_cloudwatch_event_rule.crear_docventa.arn
 }
 
+resource "aws_cloudwatch_event_bus" "procesacarrito_bus" {
+    name = "procesacarrito-bus"
+}
+
 resource "aws_cloudwatch_event_rule" "procesar_carrito" {
     name           = "procesar-carrito"
     description    = "Regla para procesar carrito desde evento personalizado"
-    event_bus_name = aws_cloudwatch_event_bus.docventas_bus.name
+    event_bus_name = aws_cloudwatch_event_bus.procesacarrito_bus.name
     event_pattern = jsonencode({
         source       = ["pe.com.jeress"],
         "detail-type": ["procesar-carrito"]
@@ -41,7 +45,7 @@ resource "aws_cloudwatch_event_target" "target_lambda_procesar_carrito" {
     rule      = aws_cloudwatch_event_rule.procesar_carrito.name
     target_id = "procesar-carrito-lambda"
     arn       = var.procesar_carrito_funcion_arn
-    event_bus_name = aws_cloudwatch_event_bus.docventas_bus.name
+    event_bus_name = aws_cloudwatch_event_bus.procesacarrito_bus.name
 }
 
 resource "aws_lambda_permission" "allow_eventbridge_procesar_carrito" {
